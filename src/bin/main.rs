7daeea5555e0f7b690ea0026df3e802a9927d292
Nalleyer <nalleyer@gmail.com>
2019-06-30 10:59:54 +0800

    add iterator for 3 kind of view

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,18 @@
 extern crate sudoku_solver;
 
-use sudoku_solver::{Sudoku};
-use std::fs;
 use std::env;
+use std::fs;
+use sudoku_solver::{Sudoku, Value};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
 
-    let contents = fs::read_to_string(file_name)
-        .expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
 
-    let s : Sudoku = Sudoku::new(&contents).unwrap();
+    let s: Sudoku = Sudoku::new(&contents).unwrap();
     println!("{}", s);
+    println!("{:?}", s.get_row(8).collect::<Vec<&Value>>());
+    println!("{:?}", s.get_column(8).collect::<Vec<&Value>>());
+    println!("{:?}", s.get_area(5).collect::<Vec<&Value>>());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,16 @@ use std::fmt;
 pub enum Value {
     Just(u8),
     Nothing,
-    Unkown(Vec<u8>)
+    Unkown(Vec<u8>),
 }
 
 #[derive(Clone, Debug)]
 pub struct Sudoku {
-    vec: Vec<Value>
+    vec: Vec<Value>,
 }
 
-
-fn make_line(line : &str) -> Result<Vec<Value>, &'static str> {
-    let mut vec : Vec<Value> = vec!();
+fn make_line(line: &str) -> Result<Vec<Value>, &'static str> {
+    let mut vec: Vec<Value> = vec![];
     for c in line.chars() {
         if c == ' ' {
             continue;
@@ -33,22 +32,68 @@ fn make_line(line : &str) -> Result<Vec<Value>, &'static str> {
     }
     if vec.len() == 9 {
         Ok(vec)
-    }
-    else {
+    } else {
         Err("parsing line failed")
     }
 }
 
 impl Sudoku {
     pub fn new(str: &str) -> Result<Sudoku, Box<dyn Error>> {
-        let mut vec : Vec<Value> = vec!();
+        let mut vec: Vec<Value> = vec![];
         for line in str.lines() {
             if !line.is_empty() {
                 vec.append(&mut make_line(line).unwrap());
             }
         }
 
-        Ok(Sudoku {vec})
+        Ok(Sudoku { vec })
+    }
+
+    // 0
+    // 1
+    // 2
+    // ...
+    // 8
+    pub fn get_row(&self, idx: usize) -> impl Iterator<Item = &Value> {
+        if idx > 8 {
+            panic!("invalid idx, getting row")
+        } else {
+            self.vec.iter().skip(idx * 9).take(9)
+        }
+    }
+
+    // 0 1 2 ... 8
+    pub fn get_column(&self, idx: usize) -> impl Iterator<Item = &Value> {
+        if idx > 8 {
+            panic!("invalid idx, getting row")
+        } else {
+            self.vec.iter().skip(idx).step_by(9)
+        }
+    }
+
+    // 0 1 2
+    // 3 4 5
+    // 6 7 8
+    pub fn get_area(&self, idx: usize) -> impl Iterator<Item = &Value> {
+        if idx > 8 {
+            panic!("invalid idx, getting row")
+        } else {
+            let big_row = idx / 3;
+            let big_column = idx % 3;
+            let row_low = big_row * 3;
+            let column_low = big_column * 3;
+            self.vec
+                .iter()
+                .enumerate()
+                .filter(move |(i, _)| {
+                    let row = i / 9;
+                    let column = i - (row * 9);
+                    let is_row_ok = row >= row_low && row < row_low + 3;
+                    let is_colulmn_ok = column >= column_low && column < column_low + 3;
+                    is_row_ok && is_colulmn_ok
+                })
+                .map(|(_, v)| v)
+        }
     }
 }
 
@@ -59,9 +104,15 @@ impl fmt::Display for Sudoku {
             write!(f, "|")?;
             for j in 0..9 {
                 match self.vec[i * 9 + j] {
-                    Value::Just(u) => { write!(f, "{}", u)?; },
-                    Value::Nothing => { write!(f, " ")?; },
-                    Value::Unkown(_) => { write!(f, "?")?; }
+                    Value::Just(u) => {
+                        write!(f, "{}", u)?;
+                    }
+                    Value::Nothing => {
+                        write!(f, " ")?;
+                    }
+                    Value::Unkown(_) => {
+                        write!(f, "?")?;
+                    }
                 }
                 if (j + 1) % 3 == 0 {
                     write!(f, "|")?;
